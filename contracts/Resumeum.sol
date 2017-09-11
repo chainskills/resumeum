7b3100cf2bc43806148cbc6867c6c9ec26230180
Said Eloudrhiri <info@noratek.net>
2017-09-11 17:48:13 +0200

    Pay to publish a resume

--- contracts/Resumeum.sol
+++ contracts/Resumeum.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.15;
 
 contract Resumeum {
      // State variables
+     address owner;
+     uint256 publishPrice;
      address consultant;
      string firstName;
      string lastName;
@@ -10,22 +12,54 @@ contract Resumeum {
      string country;
      string urlPicture;
 
+
+
      // Events
-     event createResumeEvent(
+     event publishResumeEvent(
           address indexed _consultant,
           string _firstName,
           string _lastName,
           string _country);
 
-     // create a resume
-     function createResume(
+
+     // constructor
+     function Resumeum(uint256 _publishPrice) {
+          owner = msg.sender;
+          publishPrice = _publishPrice;
+     }
+
+     // change the publication price (only for owner)
+     function setPrice(uint256 _publishPrice) {
+          require(msg.sender == owner);
+
+          publishPrice = _publishPrice;
+     }
+
+     // get the publication price
+     function getPrice() constant returns(uint256 price) {
+          return publishPrice;
+     }
+
+     // publish a resume
+     function publishResume(
           string _firstName,
           string _lastName,
           string _headline,
           string _summary,
           string _country,
-          string _urlPicture) public {
+          string _urlPicture) payable public {
 
+          // a first name and a last name are required
+          require(bytes(_firstName).length > 0);
+          require(bytes(_lastName).length > 0);
+
+          // one publication per consultant
+          require(consultant != msg.sender);
+
+          // we check whether the value sent corresponds to the service price
+          require(msg.value == publishPrice);
+
+          // store the values
           consultant = msg.sender;
           firstName = _firstName;
           lastName = _lastName;
@@ -34,7 +68,8 @@ contract Resumeum {
           country = _country;
           urlPicture = _urlPicture;
 
-          createResumeEvent(consultant, firstName, lastName, country);
+          // trigger the event
+          publishResumeEvent(consultant, firstName, lastName, country);
      }
 
      // get the resume
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Resumeum = artifacts.require("./Resumeum.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Resumeum);
+  deployer.deploy(Resumeum, 20000000000000000);
 };
